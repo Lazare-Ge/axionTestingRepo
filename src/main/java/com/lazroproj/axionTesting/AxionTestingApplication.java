fb60f13f52e38bee0bf452e11e51ab7a38be875e
lazare.giorgobiani <lazare.giorgobiani@lb.ge>
2023-06-09 21:36:34 +0400

    Initial Commit

---
+++ src/main/java/com/lazroproj/axionTesting/AxionTestingApplication.java
@@ -0,0 +1,13 @@
+package com.lazroproj.axionTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxionTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxionTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazroproj/axionTesting/AxionTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazroproj.axionTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxionTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
